a50d829fc63c7157798a580e0fb07ec61e245455
FrameTee29 <teeraphat123@hotmail.com>
2020-08-01 19:55:13 +0700

    Success JWT 100% next to signin

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,10 +2,11 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ModuleRef } from '@nestjs/core';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,private moduleRef: ModuleRef) {
     super();
   }
 
