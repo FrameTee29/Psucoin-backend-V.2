a2a4b7ae008b3270149b3744440fc8190f468256
FrameTee29 <teeraphat123@hotmail.com>
2020-08-02 23:03:32 +0700

    อธิบาย Auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,19 +9,21 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
+  async validateUser(username: string, pass: string): Promise<any> { // เงื่อนไข
+    const user = await this.usersService.findOne(username); // หา User ว่ามีไหม
+    if (user && user.password === pass) { // ถ้ามี User และ Password ตรงกัน จะเป็น True
       const { password, ...result } = user;
-      return result;
+      console.log(result);
+      return result; // return ข้อมูล
+      
     }
     return null;
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { username: user.username, sub: user.userId };  // นำ username + password มา gen เป็น token
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload), // return ในรูปแบบ JSON  ใส่ Accesss token
     };
   }
 }
